d9cec67153b398c2acea6ff15270446b9c26ec13
ChristianGarateStudent <cgara@LAPTOP-92HKL12E>
2021-09-29 20:11:35 -0400

    Branch made

--- GeneradorDeExamenes/Source.cpp
+++ GeneradorDeExamenes/Source.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include"MyMenu.h"
 using namespace std;
 
 int main() {
 //Comment C.G.
 //Test Again :)
 	cout << "Hello World!" << endl;
+	MyMenu m1;
+	m1.agregarOpcion("Word");
+	m1.agregarOpcion("Word");
+	cout << m1 << endl;
 	//TEST1
 	//TEST2
 	//TEST3
